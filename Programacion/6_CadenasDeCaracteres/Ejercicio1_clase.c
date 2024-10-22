4bec96501bb3e66c0bfbcdae23152677faa5b059
ainhoalm05 <ainhoaleomat@gmail.com>
2024-10-22 13:48:16 +0200

    Cadenas de caracteres

---
+++ Programacion/6_CadenasDeCaracteres/Ejercicio1_clase.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ *
+ *	Author:Ainhoa Leonor
+ *
+ */
+
+int main(){
+	char str1[];
+	int posicion;
+	char caracter;
+
+	printf("Introduce una palabra\n");
+	scanf("%s\n", str[]);
+	printf("Elige una posicion\n");
+	scand("%d\n", posicion);
+	printf("Elige un caracter\n");
+	scanf("%c\n", caracter);
+	
+	str1[]
+}

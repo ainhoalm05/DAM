f248f9d49f28f54b81e2d2cfacdf034cac8b9c3b
Ainhoa Leonor Matesanz <ainhoaleomat@gmail.com>
2024-11-13 14:29:27 +0100

    Fin clase punteros 13/11/2024

--- Programacion/8_Punteros/Ejercicio6_clase.c
+++ Programacion/8_Punteros/Ejercicio6_clase.c
@@ -8,8 +8,18 @@
  *
  */
 
+int incrementar(*n1,*n2){
+int num1=n1; 
+
+ *n2=n1;
+
+}
+
 int main (){
-
-
+	int n1=10;
+	int n2=20;
+	incrementar(&n1,&n2);
+	printf("%d\n",n1);
+	printf("%d\n",n2);
 	return 0;
 }
